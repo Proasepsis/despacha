11e44aa913fb505fdf621a51598cd74d9ed06359
root <root@sistemas.proasepsis.lab>
2026-06-09 15:23:58 -0500

    feat: custom admin branding + fix STORAGES for Django 5

--- despacha/settings.py
+++ despacha/settings.py
@@ -97,7 +97,14 @@ USE_I18N = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- despacha/urls.py
+++ despacha/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+admin.site.site_header = "Administración Despacha"
+admin.site.site_title  = "Despacha"
+admin.site.index_title = "Panel de administración"
+
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="lista_cortes"), name="home"),
     path("admin/", admin.site.urls),
